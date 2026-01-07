f2db5768fa7b4a8af077f6b44a7297f88164047c
Lia Ann Philip <annliaphilip@gmail.com>
2026-01-07 20:27:31 +0530

    firebase setup

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -2,20 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC7m59tMFui98Sh14kKDi6JRnvswL_VIhE",
-  authDomain: "budget-tracker-518bc.firebaseapp.com",
-  projectId: "budget-tracker-518bc",
-  storageBucket: "budget-tracker-518bc.firebasestorage.app",
-  messagingSenderId: "871794283740",
-  appId: "1:871794283740:web:e398fb8eebabcf0f801019",
-  measurementId: "G-TE03ZJBGWJ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
